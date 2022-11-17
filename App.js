ec9c3f98888af7f2836db2c9f05759adfbef2d16
Leonardo Pita <Leonardopita02@gmail.com>
2022-11-17 19:41:08 -0300

    Add playRound and gameToFive functions, there is a bug wen add +1 to the winner to be resolved

--- App.js
+++ App.js
@@ -1,6 +1,8 @@
-"use strict";
+let compWin = 0;
+let userWin = 0;
 
-function getComputerChoise() {
+
+function getComputerChoice() {
 	let compChoice = Math.floor(Math.random() * 3 + 1);
 	if (compChoice == 1) {
 		compChoice = "rock";
@@ -12,24 +14,51 @@ function getComputerChoise() {
 	return compChoice;
 }
 
-function playerSelection() {
+
+function getUserChoice() {
 	let userChoice = prompt("Rock, papper or scissors");
     userChoice = userChoice.toLowerCase();
 	if (userChoice == "rock") {
-		userChoice = "rock";
+	userChoice = "rock";
 	} else if (userChoice == "papper") {
-		userChoice = "papper";
+	userChoice = "papper";
 	} else if (userChoice == "scissors") {
-		userChoice = "scissors";
+	userChoice = "scissors";
 	} else {
 		alertInvalidSelection();
-        return
+        return userChoice;
 	}
-    console.log(userChoice);
 	return userChoice;
 }
 
 function alertInvalidSelection() {
 	alert("Wrong selection, try rock, papper or scissors only");
-	playerSelection(); 
+	getUserChoice(); 
+}
+
+function playRound (userChoice, compChoice, compWin, userWin) {
+	userChoice = getUserChoice();
+	compChoice = getComputerChoice();
+	if (userChoice == "scissors" && compChoice == "rock") {
+		return compWin = compWin + 1;
+	} else if (userChoice == "rock" && compChoice == "papper") {
+		return compWin = compWin + 1;
+	} else if (userChoice == "papper" && compChoice == "rock") {
+		return userWin = userWin + 1;
+	} else if (userChoice == "scissors" && compChoice == "papper") {
+		return userWin = userWin + 1;
+	} else if (userChoice == "rock" && compChoice == "scissors") {
+		return userWin = userWin + 1;
+	} else if (userChoice == "papper" && compChoice == "scissors") {
+		return compWin = compWin + 1;
+	} else {
+		return "Draw";
+	}
+}
+
+function gameToFive (compWin, userWin) {
+	for (let i = 0; i < 5; i++) {
+		playRound();
+		console.log(compWin, userWin);
+	}
 }
